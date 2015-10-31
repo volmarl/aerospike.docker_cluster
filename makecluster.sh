2e2bf8511a1ba33a73003bc7f5931ecfc50834b5
Lucien Volmar <lucien@aerospike.com>
2015-10-30 20:59:44 -0400

    Changed networking to multicast

--- makecluster.sh
+++ makecluster.sh
@@ -5,6 +5,6 @@ docker stop aerospike${i}
 docker rm aerospike${i}
 docker run -tid --name aerospike${i} -p ${i}000:3000 -p ${i}001:3001 -p ${i}002:3002 -p ${i}003:3003 aerospike/aerospike-server
 done;
-ipaddress=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' aerospike3)
-sleep 15
-./as_tip.sh
+#ipaddress=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' aerospike3)
+#sleep 15
+#./as_tip.sh
